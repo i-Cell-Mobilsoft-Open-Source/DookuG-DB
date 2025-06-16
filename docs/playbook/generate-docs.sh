6db46fc7e374ddea3a84e1337d03294fc2db6704
mate.biro <mate.biro@icellmobilsoft.hu>
2025-06-16 13:56:36 +0200

    19 - docs

--- docs/playbook/generate-docs.sh
+++ docs/playbook/generate-docs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# playbook file útvonala
+# path of the playbook file
 PLAYBOOK_DIR=$(dirname "$(realpath "$0")")
 
-# local antora site generálása antora cli-vel (https://docs.antora.org/antora/latest/install/install-antora/)
+# generating local antora site with antora cli (https://docs.antora.org/antora/latest/install/install-antora/)
 npx antora $PLAYBOOK_DIR/local-antora-playbook.yml
 
-# dokumentáció megnyitása böngészőben
+# opening the documentation in the browser
 xdg-open "$PLAYBOOK_DIR/build/site/index.html"
